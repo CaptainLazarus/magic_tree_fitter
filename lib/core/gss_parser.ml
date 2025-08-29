29d8da021299092f66030df9dc45c906ac505e98
CaptainLazarus <adityagudimetla@gmail.com>
2025-08-29 16:20:37 +0530

    [CL] Old GSS refactoring changes

--- lib/core/gss_parser.ml
+++ lib/core/gss_parser.ml
@@ -1,5 +1,7 @@
+open Gss_stack
 open Gss
-open Core_algo
+open Gss_actions
+open Gss_debug
 
 let update_stack_with_actions stack node_action_pairs =
   let updated_nodes =
@@ -19,32 +21,15 @@ let construct_ast (g : graph) =
     then (
       Printf.printf "RECURSION LIMIT REACHED\n%!";
       failwith "construct_ast: too many iterations");
-    Gss_debug.debug_iteration_start (List.length g.stacks);
-    (* Get actions *)
-    let new_stacks, next_actions =
-      List.map
-        (fun s ->
-           if s.direction = Forward
-           then s, get_next_actions_for_stack (get_forward_parse_table g.parse_tables) s
-           else s, get_next_actions_for_stack (get_backward_parse_table g.parse_tables) s)
-        g.stacks
-      |> List.filter (fun (_, node_action_pairs) ->
-        List.exists (fun (_, actions) -> actions <> []) node_action_pairs)
-      |> List.split
-    in
-    Gss_debug.debug_actions_found next_actions;
-    (* Update stacks *)
-    let updated_stacks = List.map2 update_stack_with_actions new_stacks next_actions in
-    let g1 = { g with stacks = updated_stacks } in
-    (* Apply actions *)
-    let final_stacks =
-      List.map2 (apply_all_actions_to_stack g1) updated_stacks next_actions
-    in
-    let g2 = { g1 with stacks = final_stacks } in
-    Gss_debug.debug_blocked_check g2.stacks;
-    Printf.printf "all_blocked check: %b\n%!" (all_blocked g2.stacks);
-    Gss_debug.debug_actions_after_apply g2.stacks;
-    if all_blocked g2.stacks then g2 else aux (iter + 1) g2
+    debug_iteration_start (List.length g.stacks);
+    let new_stacks, next_actions = get_all_actions g in
+    (* debug_actions_found next_actions; *)
+    let updated_g = update_all_stacks_with_actions g new_stacks next_actions in
+    let final_g = apply_actions_to_all_stacks updated_g updated_g.stacks next_actions in
+    (* debug_blocked_check final_g.stacks; *)
+    (* Printf.printf "all_blocked check: %b\n%!" (all_blocked final_g.stacks); *)
+    (* debug_actions_after_apply final_g.stacks; *)
+    if all_blocked final_g.stacks then final_g else aux (iter + 1) final_g
   in
   aux 0 g
 ;;
